007ece9fb082e33e6adfdce5ab722b43ab3fe4fa
hhx <827484310@qq.com>
2016-07-28 20:53:34 +0800

    modify config

--- util/searchUrl.js
+++ util/searchUrl.js
@@ -4,17 +4,20 @@ var baseUrl = 'https://www.googleapis.com/customsearch/v1element?';
 var searchInfo = {
   key: 'AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY',
   rsz: 'filtered_cse',
-  hl: 'zh_CN',
-  sort: '',
+  num: '10',
+  hl: 'en',
   prettyPrint: 'false',
   source: 'gcsc',
   gss: '.com',
-  sig: '432dd570d1a386253361f581254f9ca1',
-  cx: '002888546953948157320:bahxnr-mq7i',
+  sig: '8bdfc79787aa2b2b1ac464140255872c',
+  cx: '004953406943909708396:rsdzwgdcboa',
+  q: 'node',
+  sort: '',
   googlehost: 'www.google.com',
-  start: '0',
-  num: '10',
-  q: 'node'
+  oq: 'node',
+  gs_l: 'partner.3...1369.2692.0.2809.6.5.1.0.0.0.117.531.0j5.5.0.gsnos%2Cn%3D13...0.1331j446503j6j1..1ac.1.25.partner..6.0.0.WdQlNVr1xz0',
+  nocache: '1469708780997',
+  start: '0'
 };
 
 
